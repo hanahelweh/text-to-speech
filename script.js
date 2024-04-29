35cae2a62cd462ba7f9c99a726b4e13c92ddedb7
Hana Helweh <hanahelweh97@gmail.com>
2024-04-29 20:20:33 +0300

    some effect

---
+++ script.js
@@ -0,0 +1,40 @@
+const audio = document.getElementById("audio");
+const VoiceRSS={speech:function(e){this._validate(e),this._request(e)},_validate:function(e){if(!e)throw"The settings are undefined";if(!e.key)throw"The API key is undefined";if(!e.src)throw"The text is undefined";if(!e.hl)throw"The language is undefined";if(e.c&&"auto"!=e.c.toLowerCase()){var a=!1;switch(e.c.toLowerCase()){case"mp3":a=(new Audio).canPlayType("audio/mpeg").replace("no","");break;case"wav":a=(new Audio).canPlayType("audio/wav").replace("no","");break;case"aac":a=(new Audio).canPlayType("audio/aac").replace("no","");break;case"ogg":a=(new Audio).canPlayType("audio/ogg").replace("no","");break;case"caf":a=(new Audio).canPlayType("audio/x-caf").replace("no","")}if(!a)throw"The browser does not support the audio codec "+e.c}},_request:function(e){var a=this._buildRequest(e),t=this._getXHR();t.onreadystatechange=function(){if(4==t.readyState&&200==t.status){if(0==t.responseText.indexOf("ERROR"))throw t.responseText;
+// new Audio(t.responseText).play()
+audio.src=t.responseText;
+audio.play();
+}},t.open("POST","https://api.voicerss.org/",!0),t.setRequestHeader("Content-Type","application/x-www-form-urlencoded; charset=UTF-8"),t.send(a)},_buildRequest:function(e){var a=e.c&&"auto"!=e.c.toLowerCase()?e.c:this._detectCodec();return"key="+(e.key||"")+"&src="+(e.src||"")+"&hl="+(e.hl||"")+"&v="+(e.v||"")+"&r="+(e.r||"")+"&c="+(a||"")+"&f="+(e.f||"")+"&ssml="+(e.ssml||"")+"&b64=true"},_detectCodec:function(){var e=new Audio;return e.canPlayType("audio/mpeg").replace("no","")?"mp3":e.canPlayType("audio/wav").replace("no","")?"wav":e.canPlayType("audio/aac").replace("no","")?"aac":e.canPlayType("audio/ogg").replace("no","")?"ogg":e.canPlayType("audio/x-caf").replace("no","")?"caf":""},_getXHR:function(){try{return new XMLHttpRequest}catch(e){}try{return new ActiveXObject("Msxml3.XMLHTTP")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.6.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.3.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP")}catch(e){}try{return new ActiveXObject("Microsoft.XMLHTTP")}catch(e){}throw"The browser does not support HTTP request"}};
+
+let jokesAPI = "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
+
+//get joke from jokesAPI
+function GetJoke(event){
+    event.target.disabled=true;
+    event.target.innerHTML='Please Wait..'
+    fetch(jokesAPI).then((r)=>{
+        if(!r){
+            throw new Error("There's no response" + r.status);
+        }
+        return r.json();
+    }).then((joke)=>{
+        if(!joke.joke){
+            event.target.innerHTML='Oops! Try Again';
+        }
+        //transalet the joke from text to speech and play the audio automatically
+        VoiceRSS.speech({
+            key: '5546c11af51b41c8866eccf8fffdd231',
+            src: joke.joke,
+            hl: 'en-us',
+            v: 'Linda',
+            r: 0, 
+            c: 'mp3',
+            f: '44khz_16bit_stereo',
+            ssml: false,
+        });
+        event.target.innerHTML='<div class="dot"></div><div class="dot"></div><div class="dot"></div>';
+        audio.onended = function() {
+            event.target.innerHTML = 'One More!';
+            event.target.disabled=false;
+        };
+    }).catch(error=>console.log(error))
+}
